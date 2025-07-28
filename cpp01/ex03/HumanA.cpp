d779402aeea89285d2656b72d5fbff40425b235f
Abdallah Alawad <aalawad@c1r1s1.42amman.com>
2025-07-28 12:58:33 +0300

    understanding initializer list :)

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -5,11 +5,4 @@ void HumanA::attack()
 	std::cout << name << " attacks with their " << wa.getType() << std::endl;
 }
 
-HumanA::HumanA(const std::string &n, Weapon &w): name(n), wa(w) 
-{
-    // Constructor body (empty here)
-}
-// {
-// 	name = n;
-// 	wa = w;
-// }
+HumanA::HumanA(const std::string &n, Weapon &w): name(n), wa(w) {}
