bac0c69973717709e75980cf77f7e9a060c66306
Abdallah Alawad <aalawad@c1r6s1.42amman.com>
2025-08-11 14:35:50 +0300

    cpp02 extra practice

---
+++ cpp02/prctice_again/ex02/Fixed.hpp
@@ -0,0 +1,46 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int value;
+		static const int fraction;
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed(const int &num);
+		Fixed(const float &num);
+		Fixed &operator=(const Fixed &other);
+		~Fixed();
+		int getRawBits(void);
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+		bool operator>(const Fixed &n) const;
+		bool operator<(const Fixed &n) const;
+		bool operator>=(const Fixed &n) const;
+		bool operator<=(const Fixed &n) const;
+		bool operator!=(const Fixed &n) const;
+		Fixed operator+(const Fixed &n) const;
+		Fixed operator-(const Fixed &n) const;
+		Fixed operator*(const Fixed &n) const;
+		Fixed operator/(const Fixed &n) const;
+		Fixed &operator++();	// pre
+		Fixed operator++(int);	// post
+		Fixed &operator--();	// pre
+		Fixed operator--(int);	// post
+		static Fixed &min(Fixed &one, Fixed &two);
+		static const Fixed &min(const Fixed &one, const Fixed &two);
+		static Fixed &max(Fixed &one, Fixed &two);
+		static const Fixed &max(const Fixed &one, const Fixed &two);
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
