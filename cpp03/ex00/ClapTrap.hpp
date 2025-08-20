3b41ffb3c6ea9646ea11b3621ecbc956af9f3082
Abdallah Alawad <aalawad@c1r7s2.42amman.com>
2025-08-20 16:18:41 +0300

    ex00 cpp03 done

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,31 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+# define RED     "\033[1;31m"
+# define GREEN   "\033[1;32m"
+# define YELLOW  "\033[1;33m"
+# define BLUE    "\033[1;34m"
+# define RESET   "\033[0m"
+
+class ClapTrap
+{
+	private:
+		std::string name;
+		int hitPoints;
+		int energyPoints;
+		int attackDamage;
+	public:
+		ClapTrap();
+		ClapTrap(std::string &name);
+		ClapTrap(const ClapTrap &copy);
+		ClapTrap &operator=(const ClapTrap &obj);
+		~ClapTrap();
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
